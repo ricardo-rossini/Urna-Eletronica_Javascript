bf3f38c975117c35dad8c882632fcebe4bf31e05
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-12 22:42:00 -0300

    js vars

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let votoInfo = document.querySelector('.info-1 span');
+let cargo = document.querySelector('.info-2 span');
+let descricao = document.querySelector('.info-dados');
+let aviso = document.querySelector('.scr-2');
+let fotos = document.querySelector('.foto');
+let num = document.querySelector('.info-voto');
