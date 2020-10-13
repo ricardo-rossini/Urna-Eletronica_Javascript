dfaf138e6b6eb1a061b8484074125608fd2882ea
Ricardo <ricardo.rossini.dev@outlook.com>
2020-10-12 21:29:12 -0300

    script etapas

---
+++ js/etapas.js
@@ -0,0 +1,122 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 36111,
+                nome: 'Paula Oliveira',
+                partido: 'PAC',
+                fotos:[
+                    {url:'img/cv1.jpg', legenda: 'Vereador', small:'false'}
+                ]
+            },
+            {
+                numero: 25941,
+                nome: 'Rogerio Correia',
+                partido: 'PMN',
+                fotos:[
+                    {url:'img/cv2.jpg', legenda: 'Vereador', small:'false'}
+                ]
+            },
+            {
+                numero: 44121,
+                nome: 'Wesley Silva',
+                partido: 'PNU',
+                fotos:[
+                    {url:'img/cv3.jpg', legenda: 'Vereador', small:'false'}
+                ]
+            },
+            {
+                numero: 51333,
+                nome: 'Jurandir Massa',
+                partido: 'PHMP',
+                fotos:[
+                    {url:'img/cv4.jpg', legenda: 'Vereador', small:'false'}
+                ]
+            },
+            {
+                numero: 93111,
+                nome: 'Marco Antonio Oliveira',
+                partido: 'PBM',
+                fotos:[
+                    {url:'img/cv5.jpg', legenda: 'Vereador', small:'false'}
+                ]
+            },
+            {
+                numero: 77355,
+                nome: 'Luiz Careca',
+                partido: 'PCL',
+                fotos:[
+                    {url:'img/cv6.jpg', legenda: 'Vereador', small:'false'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                nomero: 36,
+                nome:'Marcio Bolinha',
+                partido: 'PAC',
+                vice:"Oseas",
+                fotos:[
+                    {url:'img/cp1.jpg', legenda: 'Prefeito', small: false},
+                    {url:'img/v1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                nomero: 36,
+                nome:'Marcio Bolinha',
+                partido: 'PAC',
+                vice:"Oseas",
+                fotos:[
+                    {url:'img/cp1.jpg', legenda: 'Prefeito', small: false},
+                    {url:'img/v1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                nomero: 25,
+                nome:'Aparecida Leme',
+                partido: 'PMN',
+                vice:"Cicero",
+                fotos:[
+                    {url:'img/cp2.jpg', legenda: 'Prefeito', small: false},
+                    {url:'img/v2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                nomero: 44,
+                nome:'Josue Marinho',
+                partido: 'PNU',
+                vice:"Wandercleison",
+                fotos:[
+                    {url:'img/cp3.jpg', legenda: 'Prefeito', small: false},
+                    {url:'img/v3.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                nomero: 93,
+                nome:'Francisco Manoel',
+                partido: 'PBM',
+                vice:"Eulalia",
+                fotos:[
+                    {url:'img/cp4.jpg', legenda: 'Prefeito', small: false},
+                    {url:'img/v4.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                nomero: 77,
+                nome:'Denilson Zoio',
+                partido: 'PCL',
+                vice:"Milton",
+                fotos:[
+                    {url:'img/cp1.jpg', legenda: 'Prefeito', small: false},
+                    {url:'img/v1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
